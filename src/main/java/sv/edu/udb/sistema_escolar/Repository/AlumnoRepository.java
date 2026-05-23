c6c664b535b15655fdd85247125e8454307b8f9d
Giselle2929 <gissellerodriguez046@gmail.com>
2026-05-23 12:41:07 -0600

    remove: limpiar repositorio

--- src/main/java/sv/edu/udb/sistema_escolar/Repository/AlumnoRepository.java
+++
@@ -1,7 +0,0 @@
-package sv.edu.udb.sistema_escolar.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sv.edu.udb.sistema_escolar.enity.Alumno;
-
-public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
-}
--- src/main/java/sv/edu/udb/sistema_escolar/Repository/MateriaRepository.java
+++
@@ -1,8 +0,0 @@
-package sv.edu.udb.sistema_escolar.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sv.edu.udb.sistema_escolar.enity.Materia;
-
-public interface MateriaRepository extends JpaRepository<Materia, Long> {
-}
