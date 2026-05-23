2d2e5845c6ad8964e8751b22409d0350b1ea10bf
Giselle2929 <gissellerodriguez046@gmail.com>
2026-05-22 19:40:02 -0600

    add: carpetas backend y frontend

--- src/main/java/sv/edu/udb/sistema_escolar/model/User.java
+++ src/main/java/sv/edu/udb/sistema_escolar/model/User.java
@@ -1,10 +1,14 @@
 package sv.edu.udb.sistema_escolar.model;
 
+
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -30,6 +34,12 @@ public class User implements UserDetails {
 
     @Column(nullable = false)
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
